da9da66fa936891e1446485c40ecb8bb6ae4964e
cris <rag35540@gmail.com>
2025-05-23 18:54:54 -0600

    feat: creando estilos de los items e iconos

--- app/src/main/java/com/example/sistemadecomandas/Admin/GestionUsuarioAdaptador.java
+++ app/src/main/java/com/example/sistemadecomandas/Admin/GestionUsuarioAdaptador.java
@@ -1,4 +1,32 @@
 package com.example.sistemadecomandas.Admin;
 
-public class GestionUsuarioAdaptador {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class GestionUsuarioAdaptador extends RecyclerView.Adapter<GestionUsuarioAdaptador.GestionUsuarioVIewHolder> {
+
+    @NonNull
+    @Override
+    public GestionUsuarioAdaptador.GestionUsuarioVIewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GestionUsuarioAdaptador.GestionUsuarioVIewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class GestionUsuarioVIewHolder extends RecyclerView.ViewHolder {
+        public GestionUsuarioVIewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
